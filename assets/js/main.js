b20d1fcadf1603c43ca435fb9fe586ad9fb6577d
Abu Bin Fahd <192040@ku.ac.bd>
2021-07-01 23:04:44 +0600

    qualification

--- assets/js/main.js
+++ assets/js/main.js
@@ -47,7 +47,24 @@ function toogleskills(){
 skillsHeader.forEach(e =>  e.addEventListener('click', toogleskills));
 
 /*==================== QUALIFICATION TABS ====================*/
+const tabs = document.querySelectorAll('[data-target]'),
+      tabcontents = document.querySelectorAll('[data-content]');
 
+tabs.forEach(tab => {
+    tab.addEventListener('click', () => {
+        const target = document.querySelector(tab.dataset.target)
+
+        tabcontents.forEach(tabContent => {
+            tabContent.classList.remove('qualification__active')
+        })
+        target.classList.add('qualification__active')
+
+        tabs.forEach(tab => {
+            tab.classList.remove('qualification__active')
+        })
+        tab.classList.add('qualification__active')
+    })
+})
 
 /*==================== SERVICES MODAL ====================*/
 
